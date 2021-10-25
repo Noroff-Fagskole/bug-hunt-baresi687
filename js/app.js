4a1bdb0c72125a1b03f4584cb1908d360af4c36c
baresi687 <hreinngy@gmail.com>
2021-10-25 13:53:53 +0200

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
  * your own API key at https://newsapi.org/
  * */
 
-const API_KEY = 'f8f7d5a70d8e417eadcd3916ba08997e';
+const API_KEY = 'efa4931caaaf49bc8726a0ef8cdd2c20';
 // ABC-News, Al-Jazeera-English, ars-technica, associated-press, axios, bleacher-report, bloomberg, breitbart-news, business-insider, buzzfeed, cbs-news, cnn, crypto-coins-news, engadget, entertainment-weekly, espn, fortune, fox-news, fox-sports, google-news, hacker-news, ign, mashable, medical-news-today, msnbc, mtv-news, national-geographic
 const sources = 'bbc-sport,bleacher-report,espn';
 const category = 'sports'; // business, entertainment, general, health, science, sports, technology
@@ -38,7 +38,7 @@ const renderCards = (articles) => {
             <div class="inner">
               <h2 class="title">${article.title}</h2>
               <p class="description">${article.description}</p>
-              <span class="ribbon">${article.sources.name}</span>
+              <span class="ribbon">${article.source.name}</span>
             </div>
         </div>
       `;
